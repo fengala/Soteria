c82f05280602690556e0ae734b57158da44b718d
YalmanchiliTejas <85045556+YalmanchiliTejas@users.noreply.github.com>
2023-02-22 15:59:52 -0500

    Added a user variable to the auth.dart class

--- flutter_application/lib/services/auth.dart
+++ flutter_application/lib/services/auth.dart
@@ -5,13 +5,17 @@ import 'database.dart';
 class UserAuth {
   static final FirebaseAuth auth = FirebaseAuth.instance;
 
+  static User user;
+
   static Future createUser(String email, String password, String name,
       String phone_number, List<String> emergency_contacts) async {
     FirebaseAuth auth = FirebaseAuth.instance;
     UserCredential res = await auth.createUserWithEmailAndPassword(
         email: email, password: password);
 
-    User user = res.user;
+    user = res.user;
+
+    print(user.uid);
 
     await DatabaseService(uid: user.uid)
         .register(email, password, name, emergency_contacts, phone_number);
