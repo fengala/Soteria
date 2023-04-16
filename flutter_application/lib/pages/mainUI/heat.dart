33121bd3cea7f9d30cfbcaa654efe76450b50bbe
kgovil8 <kgovil@purdue.edu>
2023-04-15 20:13:23 -0400

    Heatmap Marking

--- flutter_application/lib/pages/mainUI/heat.dart
+++ flutter_application/lib/pages/mainUI/heat.dart
@@ -20,97 +20,98 @@ class MapSample extends StatefulWidget {
 
 class MapSampleState extends State<MapSample> {
   Completer<GoogleMapController> _controller = Completer();
+  final Set<WeightedLatLng> _pins = {};
   final Set<Heatmap> _heatmaps = {};
+  LatLng _pinLocation; // Added
+
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
-  LatLng _heatmapLocation = LatLng(37.42796133580664, -122.085749655962);
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    return Scaffold(
       body: GoogleMap(
         mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
         heatmaps: _heatmaps,
+        markers: _buildMarkers(), // Added
+        onTap: _addPin,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
       ),
-      // floatingActionButton: FloatingActionButton.extended(
-      //   onPressed: _goToTheLake,
-      //   label: Text('To the lake!'),
-      //   icon: Icon(Icons.directions_boat),
-      // ),
-      // floatingActionButton: FloatingActionButton.extended(
-      //   onPressed: _addHeatmap,
-      //   label: Text('Add Heatmap'),
-      //   icon: Icon(Icons.add_box),
-      // ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          FloatingActionButton.extended(
-            onPressed: _goToTheLake,
-            label: Text('To the lake!'),
-            icon: Icon(Icons.directions_boat),
-          ),
-          SizedBox(height: 10), // Add some spacing between the buttons
-          FloatingActionButton.extended(
-            onPressed: _addHeatmap,
-            label: Text('Add Heatmap'),
-            icon: Icon(Icons.add_box),
-          ),
-          FloatingActionButton.extended(
-            onPressed: _addHeatmap,
-            label: Text('Add Heatmap'),
-            icon: Icon(Icons.add_box),
-          ),
-          FloatingActionButton.extended(
-            onPressed: _addHeatmap,
-            label: Text('Add Heatmap'),
-            icon: Icon(Icons.add_box),
-          ),
-        ],
-      ),
     );
   }
 
-  void _addHeatmap() {
+  void _addPin(LatLng latLng) {
     setState(() {
-      _heatmaps.add(Heatmap(
-          heatmapId: HeatmapId(_heatmapLocation.toString()),
-          points: _createPoints(_heatmapLocation),
-          radius: 20,
-          visible: true,
-          gradient: HeatmapGradient(
-              colors: <Color>[Colors.green, Colors.red],
-              startPoints: <double>[0.2, 0.8])));
+      _pinLocation = latLng;
     });
   }
 
-  //heatmap generation helper functions
-  List<WeightedLatLng> _createPoints(LatLng location) {
-    final List<WeightedLatLng> points = <WeightedLatLng>[];
-    //Can create multiple points here
-    points.add(_createWeightedLatLng(location.latitude, location.longitude, 1));
-    points.add(
-        _createWeightedLatLng(location.latitude - 1, location.longitude, 1));
-    return points;
+  Set<Marker> _buildMarkers() {
+    if (_pinLocation == null) {
+      return {};
+    }
+    return {
+      Marker(
+        markerId: MarkerId('pin'),
+        position: _pinLocation,
+        draggable: true,
+        onDragEnd: (LatLng newPosition) {
+          setState(() {
+            _pinLocation = newPosition;
+          });
+        },
+        onTap: () {
+          _showConfirmationDialog(_pinLocation);
+        }, // Add this line
+      ),
+    };
   }
 
   WeightedLatLng _createWeightedLatLng(double lat, double lng, int weight) {
     return WeightedLatLng(point: LatLng(lat, lng), intensity: weight);
   }
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  Heatmap _createHeatmap() {
+    return Heatmap(
+      heatmapId: HeatmapId('heatmap'),
+      points: _pins.toList(),
+    );
+  }
+
+  void _showConfirmationDialog(LatLng latLng) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Confirm"),
+          content: Text("Do you want to mark this location unsafe?"),
+          actions: <Widget>[
+            TextButton(
+              child: Text("Cancel"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text("Drop Pin"),
+              onPressed: () {
+                setState(() {
+                  _pins.add(_createWeightedLatLng(
+                      latLng.latitude, latLng.longitude, 1));
+                  _heatmaps.clear();
+                  _heatmaps.add(_createHeatmap());
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
